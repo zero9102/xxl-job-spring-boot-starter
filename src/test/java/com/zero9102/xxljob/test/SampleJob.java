a5280eef88499599cd51c0fa0e6cac5e4c3074d3
zero9102 <chloesnow2019@gmail.com>
2021-05-08 20:39:46 +0800

    modify bugs

--- src/test/java/com/zero9102/xxljob/test/SampleJob.java
+++ src/test/java/com/zero9102/xxljob/test/SampleJob.java
@@ -1,8 +1,8 @@
 package com.zero9102.xxljob.test;
 
 import com.xxl.job.core.biz.model.ReturnT;
+import com.xxl.job.core.context.XxlJobHelper;
 import com.xxl.job.core.handler.annotation.XxlJob;
-import com.xxl.job.core.log.XxlJobLogger;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,10 +13,10 @@ public class SampleJob {
      */
     @XxlJob("demoJobHandler")
     public ReturnT<String> demoJobHandler(String param) throws Exception {
-        XxlJobLogger.log("XXL-JOB, Hello World.");
+        XxlJobHelper.log("XXL-JOB, Hello World.");
 
         for (int i = 0; i < 5; i++) {
-            XxlJobLogger.log("beat at:" + i);
+            XxlJobHelper.log("beat at:" + i);
 //            TimeUnit.SECONDS.sleep(2);
         }
         return ReturnT.SUCCESS;
